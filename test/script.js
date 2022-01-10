8b0e0f48bf04fc6a54e2db092690e1798e637a9e
Francisco Damasceno <fty.57@alu.ufc.br>
2022-01-10 09:31:12 -0300

    Revisão JavaScript II

--- test/script.js
+++ test/script.js
@@ -1,3 +1,17 @@
+// Função Open
+
+function open() {
+     var container = document.getElementById('container');
+
+     var text = prompt('Digite seu nome: ');
+
+     if(text == null || text == ''){
+          alert('Digite um nome válido');
+          container.innerHTML = 'Bem vindo ...';
+     }
+
+     this.container.innerHTML = text;
+}
 
 // Este é meu nome
 // var nome = 'Álisson';
